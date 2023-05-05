923930745bd2184622fffe2b9c00956d1ef28122
EftekharAlam2 <hinsouvlmare@gmail.com>
2023-05-05 10:44:41 +0600

    Final modify

--- app.js
+++ app.js
@@ -1,19 +1,10 @@
-// calculate kitkat chocolate price with quantity
-// document
-//   .getElementById("kitkat-buy-btn")
-//   .addEventListener("click", function () {
-//     const kitkatCost = multiplePriceWithQuantity("kitkat-quantity", 200);
-//     setInnerText("chocolate", kitkatCost);
-//     document.getElementById("kitkat-quantity").value = "";
-//     total();
-//   });
-
 function handleKitkat() {
   const kitkatCost = multiplePriceWithQuantity("kitkat-quantity", 200);
   setInnerText("chocolate", kitkatCost);
   document.getElementById("kitkat-quantity").value = "";
   total();
 }
+
 // calculate rose  price with quantity
 document.getElementById("rose-buy-btn").addEventListener("click", function () {
   const roseCost = multiplePriceWithQuantity("rose-quantity", 100);
@@ -21,6 +12,7 @@ document.getElementById("rose-buy-btn").addEventListener("click", function () {
   document.getElementById("rose-quantity").value = "";
   total();
 });
+
 // calculate diary  price with quantity
 document.getElementById("diary-buy-btn").addEventListener("click", function () {
   const diaryCost = multiplePriceWithQuantity("diary-quantity", 500);
@@ -28,6 +20,7 @@ document.getElementById("diary-buy-btn").addEventListener("click", function () {
   document.getElementById("diary-quantity").value = "";
   total();
 });
+
 function total(params) {
   const chocolate = stringToNumberConverter("chocolate");
   const rose = stringToNumberConverter("rose");
@@ -35,11 +28,13 @@ function total(params) {
   const total = chocolate + rose + diary;
   setInnerText("total", total);
 }
-// pass the id of the tag this function will convert just innertext to number
+
+// pass the id of the tag this function will convert just innertext to numbe
 function stringToNumberConverter(id) {
   const element = document.getElementById(id).innerText;
   return parseFloat(element);
 }
+
 // this function will take id & value of tag and set innertext
 function setInnerText(id, value) {
   document.getElementById(id).innerText = value;
